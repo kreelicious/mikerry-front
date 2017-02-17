74c9f783a119af56ccace5ab7f59d2cb312901d8
Ideashed <ideashed@outlook.com>
2017-02-17 14:14:52 +0000

    Build
    Mikerry Rail site build

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+$(document).foundation();
+$(document).ready(function(){
+
+	// Clients Owl Carousel
+	$(".client-carousel").owlCarousel({
+
+		items:4,
+		loop: true,
+		nav: false,
+		dots: true,
+		autoPlay: true,
+		navText:[
+			'',
+			''
+		],
+		smartSpeed: 600,
+		responsive : {
+			320 : {
+				items : 1
+			},
+			768 : {
+				items : 2
+			},
+			1024 : {
+				items : 4
+			}
+		},
+
+	});
+
+	// Isotop click function
+	$('.project-grid').isotope({
+		itemSelector: '.project-item',
+		layoutMode: 'fitRows'
+	});
+
+	$('.project-nav ul li a').click(function(){
+
+		$('.project-nav ul li a').removeClass('active');
+		$(this).addClass('active');
+
+		var selector = $(this).attr('data-filter');
+
+		$('.project-grid').isotope({
+			filter: selector
+		});
+		return false;
+
+	});
+
+
+});
